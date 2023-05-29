82d503adabeb6376acf5a340428d7aaacbf39938
abhinav101901 <abhinav8299233838@gmail.com>
2023-05-29 19:11:27 +0530

    quotes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
+const quotes = [
+  "The only way to do great work is to love what you do. - Steve Jobs",
+  "Success is not the key to happiness. Happiness is the key to success. - Albert Schweitzer",
+  "Believe you can and you're halfway there. - Theodore Roosevelt",
+  "The best way to predict the future is to create it. - Peter Drucker",
+  "Innovation distinguishes between a leader and a follower. - Steve Jobs",
+  "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+  "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
+  "Don't be afraid to give up the good to go for the great. - John D. Rockefeller",
+  "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle. - Christian D. Larson",
+  "Success is not in what you have, but who you are. - Bo Bennett"
+];
+
+const App = () => {
+  const [quote, setQuote] = useState('');
+
+  const generateRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setQuote(quotes[randomIndex]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Maincontaner'>
+    <div className='contaner'>
+      <h1>Random Quote Generator</h1>
+      <button onClick={generateRandomQuote}>Generate Quote</button>
+      <div className='test'>
+        {quote}
+      </div>
+      
+    </div>
     </div>
   );
-}
+};
 
 export default App;
